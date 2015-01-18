0965404aa0f1cd709020e0d092a4dfd442a3ae88
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2015-01-18 23:04:02 +0100

    fix pathname

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,10 +13,10 @@ function App(rootEl) {
   // parse URL-Params:
   //  - audioFile
   //  - animationType
+
   this.audioPlayer_ = new AudioPlayer(this.el_);
   this.animation_ = new animations.Amp(this.audioPlayer_, {
-    loadingImage: '/images/amp-loop/00000.png',
-    filenamePattern: '/images/amp-loop/%05d.png',
+    filenamePattern: 'images/amp-loop/%05d.png',
     numImages: 79,
     numThreads: 4
   });
